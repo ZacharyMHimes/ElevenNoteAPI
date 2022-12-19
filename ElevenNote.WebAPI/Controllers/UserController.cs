51b37133e1a9385af5208e146560ddf74ccf459c
Zachary Himes <zacharymhimes@gmail.com>
2022-12-18 19:36:24 -0500

    Token Endpoint

--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -7,6 +7,8 @@ using ElevenNote.Services.User;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using ElevenNote.Services.Token;
+using ElevenNote.Models.Token;
 
 namespace ElevenNote.WebAPI.Controllers
 {
@@ -14,19 +16,21 @@ namespace ElevenNote.WebAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly IUserService _service;
-            public UserController(IUserService service)
+        private readonly IUserService _userService;
+        private readonly ITokenService _tokenService;
+        public UserController(IUserService service, ITokenService tokenService)
             {
-                _service = service;
+                _userService = service;
+                _tokenService = tokenService;
             }
-            [HttpPost("Register")]
+        [HttpPost("Register")]
         public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
             {
                 if (!ModelState.IsValid)
                 {
                     return BadRequest(ModelState);
                 }
-                var registerResult = await _service.RegisterUserAsync(model);
+                var registerResult = await _userService.RegisterUserAsync(model);
                 if (registerResult)
                 {
                     return Ok("User was registered.");
@@ -36,13 +40,24 @@ namespace ElevenNote.WebAPI.Controllers
         [Authorize]
         [HttpGet("{userId:int}")]    
         public async Task<IActionResult> GetById([FromRoute] int userId)
-        {
-            var userDetail = await _service.GetUserByIdAsync(userId);
-            if(userDetail is null)
             {
-                return NotFound();
+                var userDetail = await _userService.GetUserByIdAsync(userId);
+                if(userDetail is null)
+                {
+                    return NotFound();
+                }
+                return Ok(userDetail);
             }
-            return Ok(userDetail);
+        [HttpPost("~/api/Token")]
+        public async Task<IActionResult> Token([FromBody] TokenRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var tokenResponse = await _tokenService.GetTokenAsync(request);
+            if (tokenResponse is null)
+                return BadRequest("Invalid username and Password.");
+            return Ok(tokenResponse);
         }
     }
 }
