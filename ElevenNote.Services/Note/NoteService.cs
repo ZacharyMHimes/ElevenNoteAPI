31b408e0b33e9c3481f0acaef8ff5e181ab79a26
Zachary Himes <zacharymhimes@gmail.com>
2022-12-18 23:37:53 -0500

    GET Endpoint (17.03)

--- ElevenNote.Services/Note/NoteService.cs
+++ ElevenNote.Services/Note/NoteService.cs
@@ -1,23 +1,43 @@
+using System.Reflection.Metadata;
 using System.Security.Claims;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using ElevenNote.Models.Note;
+using ElevenNote.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Services.Note
 {
     public class NoteService : INoteService
     {
+        private readonly ApplicationDbContext _dbContext;
         private readonly int _userId;
-        public NoteService(IHttpContextAccessor httpContextAccessor)
+        public NoteService(IHttpContextAccessor httpContextAccessor, ApplicationDbContext dbContext)
         {
             var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
             var value = userClaims.FindFirst("Id")?.Value;
             var validId = int.TryParse(value, out _userId);
             if(!validId)
                 throw new Exception("Attempted to build NoteService without User Id claim.");
-        }
             
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<NoteListItem>> GetAllNotesAsync()
+        {
+            var notes = await _dbContext.Notes
+                .Where(entity => entity.OwnerId == _userId)
+                .Select(entity => new NoteListItem
+                {
+                    Id = entity.Id,
+                    Title = entity.Title,
+                    CreatedUtc = entity.CreatedUtc
+                })
+                .ToListAsync();
+            
+            return notes;
+        }    
     }
 }
